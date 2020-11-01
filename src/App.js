99454cb94ceef1748184fbfd31682df7a4cdc85e
nohsangwoo <nsgr12@naver.com>
2020-11-02 08:29:43 +0900

    plan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
-
+import Home from "./Home";
 // BEM
 function App() {
   return (
     <div className="app">
       <h1>lets build airbnb clone</h1>
+      {/* Home */}
       {/* Header */}
 
       {/* Banner */}
+      {/* Search */}
+
+      {/* Cards */}
+      {/* Footer */}
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+// ES7 snippets to do 'rfce'
+function Home() {
+  return (
+    <div className="home">
+      <h1>home components</h1>
+    </div>
+  );
+}
+
+export default Home;
