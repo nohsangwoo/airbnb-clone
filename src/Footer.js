6336364d0195a91752b33991c4a429831928aab6
nohsangwoo <nsgr12@naver.com>
2020-11-02 09:06:49 +0900

    footer  js and css

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,9 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer area</h1>
+    <div className="footer">
+      <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
     </div>
   );
 }
