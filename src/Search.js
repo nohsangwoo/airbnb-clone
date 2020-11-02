ca33c1a6fcbd64769d98b03b8f3de74cac44a1ce
nohsangwoo <nsgr12@naver.com>
2020-11-02 09:59:18 +0900

    Search.js with react-date-range usage

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { Button } from "@material-ui/core";
+import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
 
 function Search() {
-  return <div></div>;
+  const history = useHistory();
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  console.log(startDate, endDate);
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+      <h2>
+        Number of guests <PeopleIcon />
+      </h2>
+      <input min={0} defaultValue={2} type="number" />
+      <Button onClick={() => history.push("./search")}>Search Airbnb</Button>
+    </div>
+  );
 }
 
 export default Search;
