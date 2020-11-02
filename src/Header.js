cb3cd68678ef5df0d2d4558c0224c489c970e9ff
nohsangwoo <nsgr12@naver.com>
2020-11-02 11:54:34 +0900

    finish

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Header.css";
-
 import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function Header() {
+  const history = useHistory();
+  const handleForm = (e) => {
+    e.preventDefault();
+    history.push({
+      pathname: "/search",
+      search: `?query=${"submit_props"}`,
+    });
+  };
   return (
     <div className="header">
       <Link to="/">
@@ -18,10 +26,12 @@ function Header() {
         />
       </Link>
 
-      <div className="header__center">
+      <form onSubmit={handleForm} className="header__center">
         <input type="text" />
-        <SearchIcon />
-      </div>
+        <button>
+          <SearchIcon />
+        </button>
+      </form>
 
       <div className="header__right">
         <p>Become a host</p>
