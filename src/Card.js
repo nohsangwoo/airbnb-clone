3433d6d29f1aef0786a0d6673701129f460ce0f5
nohsangwoo <nsgr12@naver.com>
2020-11-02 10:10:54 +0900

    Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Card.css";
-function Card() {
-  return <div></div>;
+
+function Card({ src, title, description, price }) {
+  return (
+    <div className="card">
+      <img src={src} alt="" />
+      <div className="card__info">
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
